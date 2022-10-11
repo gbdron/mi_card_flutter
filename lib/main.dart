edb1585f84b8ab3342e58d25ffb9e9ed93e5478a
bdron-t480 <bdon@mail.ru>
2022-10-12 00:17:18 +0300

    centered Column elements

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 54, 244, 181),
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
