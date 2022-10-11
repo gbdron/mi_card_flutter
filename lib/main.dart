d9c66764f2fa38640eb18f076438067fe66e33a0
bdron-t480 <bdon@mail.ru>
2022-10-11 23:27:56 +0300

    added Circle Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 54, 244, 181),
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              SizedBox(width: 20),
-              Container(
-                height: 100,
-                width: 100,
-                color: Color.fromARGB(255, 152, 127, 182),
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Color.fromARGB(255, 196, 105, 176),
-                child: Text('Container 3'),
-              ),
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/me.jpg'),
+              )
             ],
           ),
         ),
