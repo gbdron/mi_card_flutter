d3cb62937b7290b6dc438ff8d9a043710ff9b247
bdron-t480 <bdon@mail.ru>
2022-10-11 23:58:55 +0300

    added styled Text

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,24 @@ class MyApp extends StatelessWidget {
                     fontSize: 40,
                     fontFamily: 'Pacifico',
                     fontWeight: FontWeight.bold),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                color: Colors.white54,
+                child: Row(children: [
+                  Icon(
+                    Icons.phone,
+                    size: 50,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '33443 4343 43',
+                    style: TextStyle(fontSize: 30),
+                  )
+                ]),
               )
             ],
           ),
